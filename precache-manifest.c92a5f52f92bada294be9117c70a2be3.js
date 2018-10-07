ccbcb0a98f8b54bfd2ca0569e428f2b6b70a93e7
R-Layer <layer@europe.com>
2018-10-07 11:01:21 +0200

    Updates

---
+++ precache-manifest.c92a5f52f92bada294be9117c70a2be3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/jTos-Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "163cf194c5b021a26d8f",
+    "url": "/jTos-Robofriends/static/js/runtime~main.163cf194.js"
+  },
+  {
+    "revision": "b69e84548c267d0ac375",
+    "url": "/jTos-Robofriends/static/js/main.b69e8454.chunk.js"
+  },
+  {
+    "revision": "06439e80980c9e785c16",
+    "url": "/jTos-Robofriends/static/js/1.06439e80.chunk.js"
+  },
+  {
+    "revision": "b69e84548c267d0ac375",
+    "url": "/jTos-Robofriends/static/css/main.31f07d3d.chunk.css"
+  },
+  {
+    "revision": "06439e80980c9e785c16",
+    "url": "/jTos-Robofriends/static/css/1.6b3d1ec7.chunk.css"
+  },
+  {
+    "revision": "a2a6f7374c9c656dc451460ad1f79d79",
+    "url": "/jTos-Robofriends/index.html"
+  }
+];
